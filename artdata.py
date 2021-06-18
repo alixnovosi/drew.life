f8690fa5e36c3f8d4b12722c5751130dfa226b3c
Andrew Michaud <dev@drew.life>
2021-06-18 10:15:09 -0700

    fix subject

--- artdata.py
+++ artdata.py
@@ -75,7 +75,7 @@ ART_DATA = [
         "link": "/gallery/commart/self-made self-ref 1.0 June 2021.png",
         "alt": "Alix Novosi Reference Sheet",
         "title": "Alix Novosi Reference Sheet",
-        "subject": "Alix Novosi",
+        "subject": "Alix (fursona)",
         "artist": {
             "link": "https://twitter.com/alixnovosi",
             "name": "Alix Novosi",
